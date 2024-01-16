49d8ba468e8947a26a807240a1f50d300976d84b
vketteni <vketteni@vketteni42.myguest.virtualbox.org>
2024-01-16 18:25:17 +0100

    Jan 16 24

--- push_swap.c
+++ push_swap.c
@@ -13,7 +13,7 @@
 #include "push_swap.h"
 
 // int main(void)
-int main(int argc, char** argv)
+int	main(int argc, char **argv)
 {
 	t_dlist	**stack_a;
 	t_dlist	**stack_b;
@@ -21,106 +21,99 @@ int main(int argc, char** argv)
 	if (argc < 2)
 		return (0);
 	ft_init_stack(stack_a, argc, argv);
-    ft_print_operations(stack_a, stack_b, argc - 1);
-
-    return (0);
+	ft_print_operations(stack_a, stack_b, argc - 1);
+	return (0);
 }
 
-void    ft_print_operations(t_dlist **stack_a, t_dlist **stack_b, int unordered)
+void	ft_print_operations(t_dlist **stack_a, t_dlist **stack_b, int unordered)
 {
-    char	**sort_operations;
-    int		*sorted_top;
-	int		*sorted_bottom;
+	char	**sort_operations;
+	int		*last_lowest;
+	int		*last_highest;
 
-	sorted_top = *stack_a;
-	sorted_bottom = *stack_a;
-    while (unordered > 0)
-    {
+	last_lowest = *stack_a;
+	last_highest = *stack_a;
+	while (unordered > 0)
+	{
 		if (1)
-			sort_operations = ft_selection_sort(stack_a, stack_b, sorted_bottom, sorted_top);
+			sort_operations = ft_selection_sort(stack_a, stack_b, last_highest,
+					last_lowest);
 		if (sort_operations == NULL)
 			return ((void)ft_printf("Error \n"));
 		ft_printf(sort_operations);
 		unordered--;
-    }
-}
-
-t_dlist	*ft_select_upwards(t_dlist **stack, t_dlist *last, int distance)
-{
-	t_dlist	*next_sorted;
-
-	next_sorted = last;
-	while (distance)
-	{
-		if (next_sorted->next == NULL)
-			next_sorted = *stack;
-		else
-			next_sorted = next_sorted->next;
-		distance--;
 	}
-	return (next_sorted);
 }
 
-t_dlist	*ft_select_backwards(t_dlist **stack, t_dlist *last, int distance)
+void	ft_swap_next_lowest(t_dlist *last, t_dlist *next_lowest)
 {
-	t_dlist	*next_sorted;
+	t_dlist	*before_lowest;
+	t_dlist	*after_lowest;
 
-	next_sorted = last;
-	while (distance)
+	before_lowest = next_lowest->prev;
+	before_lowest->next = next_lowest->next;
+	after_lowest = next_lowest->next;
+	after_lowest->prev = next_lowest->prev;
+	last->prev = next_lowest;
+	next_lowest->next = last;
+	next_lowest->prev = NULL;
+}
+
+void	ft_swap_next_highest(t_dlist *last, t_dlist *next_highest)
+{
+	t_dlist	*tmp1;
+	t_dlist	*tmp2;
+
+	if (last->next == NULL)
+		return ;
+	tmp1 = last->next;
+	tmp2 = next_highest;
+	next_highest->next = tmp1->next;
+	next_highest->prev = tmp1->prev;
+	(last->next)->next = tmp2->next;
+	(last->next)->prev = tmp2->prev;
+}
+
+char	*ft_selection_sort(t_dlist **stack_a, t_dlist **stack_b,
+		t_dlist *last_highest, t_dlist *last_lowest)
+{
+	int		distance;
+	t_dlist	*next;
+
+	distance = ft_next_distance(last_lowest, last_highest, next);
+	if (next == 0 || distance == 0)
+		return (0);
+	if (next->content < last_lowest->content)
+		ft_swap_next_lowest(last_lowest, next);
+	ft_swap_next_highest(last_highest, next);
+	return (ft_selection_sort_operations(last_lowest, last_highest, next,
+			distance));
+}
+
+int	ft_next_distance(t_dlist *last_lowest, t_dlist *last_highest, t_dlist *next)
+{
+	t_dlist	*tail;
+	t_dlist	*head;
+	int		distance;
+
+	head = last_highest->next;
+	tail = ft_dlstlast(last_highest);
+	distance = 1;
+	while (head != tail)
 	{
-		if (next_sorted->prev == NULL)
-			next_sorted = ft_dlstlast(*stack);
-		else
-			next_sorted = next_sorted->prev;
-		distance--;
+		if (tail->content < last_lowest || tail->content > last_highest)
+		{
+			next = tail;
+			return (-distance);
+		}
+		if (head->content < last_lowest || head->content > last_highest)
+		{
+			next = head;
+			return (distance);
+		}
+		distance++;
+		tail = tail->prev;
+		head = head->prev;
 	}
-	return (next_sorted);
-}
-
-void	ft_swap_next(t_dlist *unsorted_elem, t_dlist *next_sorted)
-{
-	t_dlist *adjacent_elem;
-	t_dlist *tmp;
-
-		tmp = unsorted_elem;
-		if (unsorted_elem == NULL)
-		next_sorted->next = unsorted_elem->next;	 
-		adjacent_elem = next_sorted->next;
-		if (adjacent_elem != NULL)
-			adjacent_elem->prev = next_sorted->prev;
-		
-		adjacent_elem = last->prev;
-		next_sorted->prev = adjacent_elem;
-		if (adjacent_elem != NULL)
-			adjacent_elem->next = next_sorted;
-		last->prev = next_sorted;
-}
-
-
-char *ft_select_and_swap(t_dlist **stack_a, t_dlist *last, int distance)
-{
-	char 	*sort_operations;
-	t_dlist	*next_sorted;
-
-	if (distance < 0)
-		next_sorted = ft_select_backwards(stack_a, last, -distance);
-	else
-		next_sorted = ft_select_upwards(stack_a, last, distance);
-	if (last->content > next_sorted->content)
-		ft_swap_next(last->prev, next_sorted);
-	else
-		ft_swap_next_top(last, next_sorted);	
-	return (ft_selection_sort_operations(distance));
-}
-
-char	*ft_selection_sort(t_dlist **stack_a, t_dlist **stack_b, t_dlist *sorted_top, t_dlist *sorted_bottom)
-{
-	int	distance_new_bottom;
-	int	distance_new_top;
-
-	ft_init_next_distance(distance_new_bottom, distance_new_top, sorted_bottom, sorted_top);
-	if (distance_new_top <= distance_new_bottom)
-		return (ft_select_and_swap(stack_a, sorted_top, distance_new_top));
-	else
-		return (ft_select_and_swap(stack_a, sorted_bottom, distance_new_bottom));
+	return (0);
 }
